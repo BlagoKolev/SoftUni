0f2e58e7977f0f0670e91cec1b7491f5cec33fa0
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-12 23:55:16 +0300

    Homework

---
+++ OOP/Inheritance/Person/Child.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Child : Person
+    {
+
+        private int age;
+
+        public Child(string name, int age)
+            : base(name, age)
+        {
+
+        }
+
+        public override int Age
+        {
+            get { return base.Age; }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Child's age must be less than 15!");
+                }
+                base.Age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
+
---
+++ OOP/Inheritance/Person/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Person
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            try
+            {
+                Child child = new Child(name, age);
+                Console.WriteLine(child);
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+        }
+    }
+}
