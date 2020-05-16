c0eaa8d4076529f87a328c46049b85c3b2ccc7dc
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-16 08:18:12 +0300

    Homework

---
+++ OOP/InterfacesAndAbstraction/DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,25 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+    }
+}
