7cae7abe20ccef0d1b617787c176ed6eecf163e0
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-27 20:15:05 +0300

    Homework

---
+++ C# Advanced/Sets and Dictionaries Advanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split();
+            var n = int.Parse(numbers[0]);
+            var m = int.Parse(numbers[1]);
+            var firstSet = new List<int>();
+            var secondSet = new List<int>();
+           
+
+            for (int i = 0; i < n + m; i++)
+            {
+                var number = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    firstSet.Add(number);
+
+                }
+                else
+                {
+                    secondSet.Add(number);
+                }
+            }
+
+            
+            Console.WriteLine(string.Join(" ", firstSet.Intersect(secondSet)));
+        }
+    }
+}
--- C# Advanced/Sets and Dictionaries Advanced/06.Wardrobe/Program.cs
+++ C# Advanced/Sets and Dictionaries Advanced/06.Wardrobe/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _06.Wardrobe
 {
@@ -6,7 +9,63 @@ namespace _06.Wardrobe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = int.Parse(Console.ReadLine());
+
+            var wardrobe = new Dictionary<string, Dictionary<string, int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine()
+                    .Split(new string[] { " -> " }
+                    , StringSplitOptions
+                    .RemoveEmptyEntries);
+
+                var color = input[0];
+                var clothes = input[1]
+                    .Split(new char[] { ',' }
+                    , StringSplitOptions
+                    .RemoveEmptyEntries);
+
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe[color] = new Dictionary<string, int>();
+                }
+
+                for (int j = 0; j < clothes.Length; j++)
+                {
+                    var currentDress = clothes[j];
+
+                    if (!wardrobe[color].ContainsKey(currentDress))
+                    {
+                        wardrobe[color][currentDress] = 0;
+                    }
+                    wardrobe[color][currentDress] += 1;
+                }
+
+
+            }
+            var wantedStuff = Console.ReadLine().Split();
+            var wantedColor = wantedStuff[0];
+            var wantedDress = wantedStuff[1];
+
+
+            foreach (var color in wardrobe)
+            {
+                Console.WriteLine("{0} clothes:", color.Key);
+
+                foreach (var clothes in color.Value)
+                {
+                    if (color.Key == wantedColor && clothes.Key == wantedDress)
+                    {
+                        Console.WriteLine("* {0} - {1} (found!)", clothes.Key, clothes.Value);
+                    }
+                    else
+                    {
+                        Console.WriteLine("* {0} - {1}", clothes.Key, clothes.Value);
+                    }
+
+                }
+            }
         }
     }
 }
