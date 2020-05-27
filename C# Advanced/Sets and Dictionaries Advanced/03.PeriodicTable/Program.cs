01edf769e13eaefe1dfa01906092474973a76f56
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-27 19:42:14 +0300

    Homework

---
+++ C# Advanced/Sets and Dictionaries Advanced/03.PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var listOfElements = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var elements = Console.ReadLine().Split();
+
+                foreach (var element in elements)
+                {
+                    listOfElements.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", listOfElements));
+        }
+    }
+}
