ab1cea4c5106b1f6ed2b2f06a67b666b3f9a4a7e
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-27 20:03:53 +0300

    Homework

---
+++ C# Advanced/Sets and Dictionaries Advanced/06.CountSymbols/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+
+            var myDictionary = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var symbol = text[i];
+
+                if (!myDictionary.ContainsKey(symbol))
+                {
+                    myDictionary[symbol] = 0;
+                }
+                myDictionary[symbol] += 1;
+            }
+
+            foreach (var symbol in myDictionary)
+            {
+                Console.WriteLine("{0}: {1} time/s", symbol.Key, symbol.Value);
+            }
+        }
+    }
+}
