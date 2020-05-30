354487f5b18e641117e24686e56574cf013a927c
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-30 09:43:51 +0300

    Homework

---
+++ C# Advanced/Functional Programing/03.CustomMinFunction/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.CustomMinFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> FindMinNumber = x =>
+             {
+                 var minValue = int.MaxValue;
+
+                 foreach (var number in x)
+                 {
+                     if (number < minValue)
+                     {
+                         minValue = number;
+                     }
+                 }
+                 return minValue;
+             };
+
+            var array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(FindMinNumber(array));
+
+        }
+    }
+}
