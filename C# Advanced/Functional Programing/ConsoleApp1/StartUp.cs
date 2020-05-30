6f8c5e1074002011b6283a0b6fa397bdc4fff2ed
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-30 09:14:41 +0300

    Homework

---
+++ C# Advanced/Functional Programing/ConsoleApp1/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FunctionalPrograming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            
+            Action<string> print = x => Console.WriteLine(x);
+
+          Console.ReadLine().Split().ToList().ForEach(print);
+
+        }
+       
+    }
+}
