ef88b1b67821a006b3e622cfca9ddfceda9270f3
BlagoKolev <blagovest.kolev@gmail.com>
2020-05-30 09:26:19 +0300

    Homework

---
+++ C# Advanced/Functional Programing/02.KnightsOfHonour/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.KnightsOfHonour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> print = x => Console.WriteLine($"Sir {x}");
+
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToList().ForEach(print);
+                
+                
+        }
+    }
+}
