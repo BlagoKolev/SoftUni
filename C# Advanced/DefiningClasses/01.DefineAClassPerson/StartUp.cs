7b9e8e78ccf609f78dc9e7d673e31ee04207f679
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-03 06:45:50 +0300

    Homework

---
+++ C# Advanced/DefiningClasses/01.DefineAClassPerson/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age =int.Parse(Console.ReadLine());
+            var person = new Person();
+            person.Name = name;
+            person.Age = age;
+            Console.WriteLine($"{person.Name} {person.Age}");
+            
+        }
+    }
+}
