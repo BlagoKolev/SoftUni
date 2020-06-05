f2a39a69435363458b516cc21b8f008c1e852cf9
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-05 05:21:16 +0300

    Homework

---
+++ C# Advanced/DefiningClasses/04.OpinionPoll/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> members;
+
+        public Family()
+        {
+            this.Members = new List<Person>();
+        }
+
+        public List<Person> Members
+        {
+            get { return this.members; }
+            private set { this.members = value; }
+        }
+
+        public void AddMember(Person person)
+        {
+            this.Members.Add(person);
+        }
+
+        public List<Person> GetMembersOlderThan30()
+        {
+            var olderThan30 = this.Members.Where(x => x.Age > 30).ToList();
+            return olderThan30;
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var name = input[0];
+                var age = int.Parse(input[1]);
+
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            var olderThan30 = family.GetMembersOlderThan30().OrderBy(x => x.Name);
+            Console.WriteLine(string.Join(Environment.NewLine,
+                olderThan30));
+        }
+    }
+}
