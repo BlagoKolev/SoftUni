c8828d6219169c56e4df41f97bc30e22dd5ee090
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-05 05:29:57 +0300

    Homework

---
+++ C# Advanced/DefiningClasses/06.SpeedRacing/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKm;
+        private double travelledDistance = 0;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKm = fuelConsumptionPerKm;
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            private set { this.model = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return this.fuelAmount; }
+            private set { this.fuelAmount = value; }
+        }
+
+        public double FuelConsumptionPerKm
+        {
+            get { return this.fuelConsumptionPerKm; }
+            private set { this.fuelConsumptionPerKm = value; }
+        }
+
+        public double TravelledDistance
+        {
+            get { return this.travelledDistance; }
+            set { this.travelledDistance = value; }
+        }
+
+        public void Move(double kilometers)
+        {
+            var neededFuel = this.fuelConsumptionPerKm * kilometers;
+
+            if (this.FuelAmount >= neededFuel)
+            {
+                this.FuelAmount -= neededFuel;
+                this.TravelledDistance += kilometers;
+            }
+            else
+            {
+                System.Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append(string.Concat(this.Model + ' ' + this.FuelAmount + ' ' + this.FuelConsumptionPerKm));
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/DefiningClasses/06.SpeedRacing/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var cars = new List<Car>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var carArgs = Console.ReadLine().Split();
+
+                var model = carArgs[0];
+                var fuelAmount = double.Parse(carArgs[1]);
+                var fuelConsumptionPerKm = double.Parse(carArgs[2]);
+
+                var car = new Car(model, fuelAmount, fuelConsumptionPerKm);
+                cars.Add(car);
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+
+                if (command[0] == "End")
+                {
+                    break;
+                }
+
+                var carModel = command[1];
+                var amountOfKm = double.Parse(command[2]);
+
+                var currentCar = cars.Where(x => x.Model == carModel).FirstOrDefault();
+
+                currentCar.Move(amountOfKm);
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine("{0} {1:f2} {2}", car.Model, car.FuelAmount, car.TravelledDistance);
+            }
+        }
+    }
+}
