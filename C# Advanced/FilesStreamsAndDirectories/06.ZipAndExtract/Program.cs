4f05a60783b9a1f6b7c7bad8baf3d4d146d632a5
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-08 19:46:49 +0300

    Homework

---
+++ C# Advanced/FilesStreamsAndDirectories/06.ZipAndExtract/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO.Compression;
+using System.Text;
+using System.IO;
+
+namespace _06.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fileName = "copyMe.png";
+            var pathResult = (@"..\..\..\HomeworkFolder\copyMe.zip"); 
+            var destinationUnzip = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            using (var archive = ZipFile.Open(pathResult, ZipArchiveMode.Create)) //creating an empty .zip file
+            {
+                archive.CreateEntryFromFile(fileName, Path.GetFileName(fileName));      //add an wanted file to out .zip archive
+            }
+
+            ZipFile.ExtractToDirectory(pathResult, destinationUnzip);
+
+
+
+        }
+    }
+}
