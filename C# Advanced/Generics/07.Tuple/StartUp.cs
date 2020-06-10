32c0e96b75589bf6e177d2ab4371ba4480c99276
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-10 06:53:57 +0300

    Homework

---
+++ C# Advanced/Generics/07.Tuple/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+            var input = Console.ReadLine().Split();
+            var fullName = input[0] + ' '  + input[1];
+            var adress = input[2];
+            var tuple = new Tuple<string, string>(fullName, adress);
+            Console.WriteLine(tuple);
+
+            var input2 = Console.ReadLine().Split();
+            var name = input2[0];
+            var beer = int.Parse(input2[1]);
+            var tuple2 = new Tuple<string, int>(name, beer);
+            Console.WriteLine(tuple2);
+
+            var input3 = Console.ReadLine().Split();
+            var integer = int.Parse(input3[0]);
+            var doublee = double.Parse(input3[1]);
+            var tuple3 = new Tuple<int, double>(integer, doublee);
+            Console.WriteLine(tuple3);
+        }
+    }
+}
---
+++ C# Advanced/Generics/07.Tuple/Tuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class Tuple<T1, T2>
+    {
+        public Tuple(T1 firstItem, T2 secondItem)
+        {
+            this.Item1 = firstItem;
+            this.Item2 = secondItem;
+        }
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public override string ToString()
+        {
+
+            var result = Item1 + " -> " + Item2;
+            return result;
+        }
+    }
+}
