128fdf9a785e3140d656b690d8cf82c7d0e949a0
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-10 06:44:51 +0300

    Homework

---
+++ C# Advanced/Generics/05.GenericCountMethodString/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var myList = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                myList.Add(input);
+
+            }
+            var box = new Box<string>();
+            var elementTocompare = Console.ReadLine();
+
+
+            Console.WriteLine(box.FindBiggerElements(myList, elementTocompare));
+
+        }
+    }
+}
