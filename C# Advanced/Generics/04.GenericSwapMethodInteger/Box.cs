6575a4aad4d07cf1354a622ac76cd6986fd3c735
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-10 06:42:19 +0300

    Homework

---
+++ C# Advanced/Generics/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T>
+    {
+        private List<T> value;
+
+        public Box(List<T> value)
+        {
+            this.Value = value;
+        }
+
+        public List<T> Value
+        {
+            get { return this.value; }
+            private set { this.value = value; }
+        }
+
+        public List<T> SwapElements<T>(List<T> someList, int index1, int index2)
+        {
+            var helperVariable = someList[index1];
+            someList[index1] = someList[index2];
+            someList[index2] = helperVariable;
+            return someList;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in this.Value)
+            {
+                var type = item.GetType();
+                sb.AppendLine(string.Format("{0}: {1}", type, item));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ C# Advanced/Generics/04.GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = int.Parse(Console.ReadLine());
+                numbers.Add(input);
+            }
+            var box = new Box<int>(numbers);
+
+            var inputIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var index1 = inputIndexes[0];
+            var index2 = inputIndexes[1];
+            var output = box.SwapElements(numbers, index1, index2);
+
+
+            Console.WriteLine(box);
+        }
+
+    }
+}
