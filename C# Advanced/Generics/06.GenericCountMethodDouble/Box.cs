5955f0a1c2ebe77e4fad2fee4fa84819d2fd1be3
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-10 06:47:33 +0300

    Homework

---
+++ C# Advanced/Generics/06.GenericCountMethodDouble/Box.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> myList;
+
+        public Box()
+        {
+            this.MyList = new List<T>();
+        }
+
+        public List<T> MyList
+        {
+            get { return this.myList; }
+            private set { this.myList = value; }
+        }
+
+        public int FindBiggerElements(List<T> myList, T element)
+        {
+            var count = 0;
+
+            foreach (var item in myList)
+            {
+                if (element.CompareTo(item) == -1)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+    }
+}
---
+++ C# Advanced/Generics/06.GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Generics
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var myList = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var input = double.Parse(Console.ReadLine());
+                myList.Add(input);
+
+            }
+            var box = new Box<double>();
+            var elementTocompare = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(box.FindBiggerElements(myList, elementTocompare));
+
+        }
+    }
+}
+
