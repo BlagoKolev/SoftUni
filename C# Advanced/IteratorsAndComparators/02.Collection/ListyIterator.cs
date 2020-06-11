656debab89bba2b972291851889ac1645df33617
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-11 06:40:17 +0300

    Homework

---
+++ C# Advanced/IteratorsAndComparators/02.Collection/ListyIterator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> myList;
+        private int pointer = 0;
+
+        public ListyIterator(params T[] array)
+        {
+            this.myList = new List<T>(array);
+        }
+
+        public int Pointer
+        {
+            get { return this.pointer; }
+            private set { this.pointer = value; }
+        }
+
+        public bool Move()
+        {
+            if (this.Pointer >= this.myList.Count-1)
+            {
+                return false;
+            }
+            this.Pointer++;
+            return true;
+        }
+
+        public bool HasNext()
+        {
+            if (this.pointer >= this.myList.Count - 1)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void Print()
+        {
+            if (this.myList.Count == 0)
+            {
+                Console.WriteLine("Invalid Operation!");
+                return;
+            }
+            Console.WriteLine(this.myList[this.Pointer]);
+        }
+
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.myList.Count; i++)
+            {
+                yield return this.myList[i];
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/02.Collection/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> iterator = null;
+            while (true)
+            {
+                var input = Console.ReadLine().Split();
+
+                if (input[0] == "END")
+                {
+                    return;
+                }
+
+                if (input[0] == "Create")
+                {
+                    if (input.Length == 1)
+                    {
+                        iterator = new ListyIterator<string>();
+                    }
+                    else
+                    {
+                        var listArgs = input.Skip(1).ToArray();
+                        iterator = new ListyIterator<string>(listArgs);
+                    }
+                }
+
+                else if (input[0] == "Move")
+                {
+                    Console.WriteLine(iterator.Move());
+                }
+                else if (input[0] == "Print")
+                {
+                    iterator.Print();
+                }
+                else if (input[0] == "HasNext")
+                {
+                    Console.WriteLine(iterator.HasNext());
+                }
+                else if (input[0] == "PrintAll")
+                {
+                    Console.WriteLine(string.Join(" ", iterator));
+                }
+            }
+        }
+    }
+}
