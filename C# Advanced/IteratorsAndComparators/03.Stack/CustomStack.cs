e56e8dd70025e5fb2b7a27a4f569a3d594511b2f
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-12 02:19:32 +0300

    Homework

---
+++ C# Advanced/IteratorsAndComparators/03.Stack/CustomStack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class CustomStack<T> : IEnumerable<T>
+    {
+        private List<T> myCustomStack;
+
+        private int pointer = -1;
+
+        public CustomStack()
+        {
+            this.MyCustomStack = new List<T>();
+        }
+
+        public List<T> MyCustomStack
+        {
+            get { return this.myCustomStack; }
+            private set { this.myCustomStack = value; }
+        }
+
+        public int Pointer
+        {
+            get { return this.pointer; }
+            private set { this.pointer = value; }
+        }
+
+
+        public void Push(params T[] elements)
+        {
+            foreach (var element in elements)
+            {
+                this.MyCustomStack.Add(element);
+                this.Pointer++;
+            }
+        }
+
+        public void Pop()
+        {
+            if (pointer < 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+            this.MyCustomStack.RemoveAt(pointer);
+            this.Pointer--;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+
+
+            for (int i = pointer; i >= 0; i--)
+            {
+                yield return this.MyCustomStack[i];
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/03.Stack/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStack = new CustomStack<string>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Split(new char[] { ',', ' ' }
+                , StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "END")
+                {
+                    break;
+                }
+
+                if (input[0] == "Push")
+                {
+                    var elements = input.Skip(1).ToArray();
+                    myStack.Push(elements);
+                }
+                else if (input[0] == "Pop")
+                {
+                    try
+                    {
+                        myStack.Pop();
+                    }
+                    catch (InvalidOperationException ioe)
+                    {
+                        Console.WriteLine(ioe.Message);
+                    }
+                }
+            }
+
+            if (myStack.MyCustomStack.Count > 0)
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    foreach (var element in myStack)
+                    {
+                        Console.WriteLine(element);
+                    }
+                }
+            }
+
+        }
+    }
+}
