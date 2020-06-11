1987b760873d80a2f6cbb0fd2f2527da95d65b26
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-12 02:20:03 +0300

    Homework

---
+++ C# Advanced/IteratorsAndComparators/04.Froggy/Lake.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndcomparators
+{
+    public class Lake : IEnumerable<int>
+    {
+
+        private List<int> stones;
+
+        public Lake(params int[] numbers)
+        {
+            this.Stones = new List<int>(numbers);
+        }
+        public List<int> Stones
+        {
+            get { return this.stones; }
+            private set { this.stones = value; }
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            var evenNum = new List<int>();
+            var oddNum = new List<int>();
+
+            for (int i = 0; i < stones.Count; i++)
+            {
+                var element = stones[i];
+                if (i % 2 == 0)
+                {
+                    evenNum.Add(element);
+                }
+                else
+                {
+                    oddNum.Add(element);
+                }
+            }
+            oddNum.Reverse();
+            evenNum.AddRange(oddNum);
+            foreach (var item in evenNum)
+            {
+                yield return item;
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ C# Advanced/IteratorsAndComparators/04.Froggy/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndcomparators
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(new char[] { ',', ' ' }
+                , StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var lake = new Lake(numbers);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
