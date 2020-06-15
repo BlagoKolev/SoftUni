a576a47993511bbec54c255a1319d6e27985978c
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-15 19:47:50 +0300

    Exam Preparation

--- C# Advanced/ExamPreparation/01.ClubParty/Program.cs
+++ C# Advanced/ExamPreparation/01.ClubParty/Program.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace _01.ClubParty
+namespace ClubParty
 {
-    class Program
+    class StartUp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var maxCapacity = int.Parse(Console.ReadLine());
+
+            var input = Console.ReadLine().Split();
+            var reservations = new Stack<string>(input);
+            var sb = new StringBuilder();
+            var sum = 0;
+            var hall = new Queue<string>();
+            var hallsGuest = new List<int>();
+
+            var currentHall = string.Empty;
+
+            while (reservations.Any())
+            {
+                var currentElement = reservations.Peek();
+                var number = 0;
+                var isNumber = int.TryParse(currentElement, out number);
+
+                if (!isNumber)
+                {
+                    currentHall = currentElement;
+                    hall.Enqueue(currentHall);
+                    reservations.Pop();
+
+                    continue;
+                }
+                else
+                {
+                    if (hall.Count == 0)
+                    {
+                        reservations.Pop();
+                        continue;
+                    }
+                    if (sum + number <= maxCapacity)
+                    {
+                        sum += number;
+                        hallsGuest.Add(number);
+                        reservations.Pop();
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0} -> " + string.Join(", ", hallsGuest), hall.Dequeue());
+                        sum = 0;
+                        hallsGuest.Clear();
+
+                    }
+
+                }
+            }
         }
     }
 }
