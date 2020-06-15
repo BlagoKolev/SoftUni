9149b1d403c68973a5f71ebe7f7922a48c7d341f
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-15 19:44:57 +0300

    Exam Preparation

---
+++ C# Advanced/ExamPreparation/01.ClubParty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01.ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
