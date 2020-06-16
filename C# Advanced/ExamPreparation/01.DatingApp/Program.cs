0f6bb91f6c35962d2850f1d2f037f6e9165bc5b0
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-16 20:42:31 +0300

    Exam Preparation

---
+++ C# Advanced/ExamPreparation/01.DatingApp/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DatingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var males = ReadInput(Console.ReadLine());
+            var females = ReadInput(Console.ReadLine());
+
+            var maleStack = new Stack<int>(males);
+            var femaleQueue = new Queue<int>(females);
+            var matchesCount = 0;
+
+            while (maleStack.Any() && femaleQueue.Any())
+            {
+                var male = maleStack.Peek();
+                var female = femaleQueue.Peek();
+
+                if (female <= 0)
+                {
+                    femaleQueue.Dequeue();
+                    if (!femaleQueue.Any())
+                    {
+                        break;
+                    }
+                    //female = femaleQueue.Peek();
+                    continue;
+                }
+                if (male <= 0)
+                {
+                    maleStack.Pop();
+                    if (!maleStack.Any())
+                    {
+                        break;
+                    }
+                    male = maleStack.Peek();
+                }
+
+                if (female % 25 == 0)
+                {
+                    femaleQueue.Dequeue();
+
+                    if (!femaleQueue.Any())
+                    {
+                        break;
+                    }
+                    femaleQueue.Dequeue();
+                    if (!maleStack.Any())
+                    {
+                        break;
+                    }
+                    female = femaleQueue.Peek();
+
+                }
+                if (male % 25 == 0)
+                {
+                    maleStack.Pop();
+
+                    if (!maleStack.Any())
+                    {
+                        break;
+                    }
+                    maleStack.Pop();
+                    if (!maleStack.Any())
+                    {
+                        break;
+                    }
+                    male = maleStack.Peek();
+
+                }
+
+
+                if (male == female)
+                {
+                    matchesCount++;
+                    maleStack.Pop();
+                    femaleQueue.Dequeue();
+                }
+                else
+                {
+                    femaleQueue.Dequeue();
+                    male = maleStack.Pop();
+                    male -= 2;
+                    if (male <= 0)
+                    {
+                        continue;
+                    }
+                    maleStack.Push(male);
+                }
+            }
+
+            Console.WriteLine("Matches: {0}", matchesCount);
+            if (maleStack.Any())
+            {
+                Console.WriteLine("Males left: {0}", string.Join(", ", maleStack));
+            }
+            else
+            {
+                Console.WriteLine("Males left: none");
+            }
+            if (femaleQueue.Any())
+            {
+                Console.WriteLine("Females left: {0}", string.Join(", ", femaleQueue));
+            }
+            else
+            {
+                Console.WriteLine("Females left: none");
+            }
+        }
+        public static int[] ReadInput(string input)
+        {
+            return input.Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+    }
+}
--- C# Advanced/ExamPreparation/02.TronRacers/Program.cs
+++ C# Advanced/ExamPreparation/02.TronRacers/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace _02.TronRacers
 {
@@ -6,7 +9,209 @@ namespace _02.TronRacers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var size = int.Parse(Console.ReadLine());
+            var rows = size;
+            var cols = size;
+            var matrix = new char[rows, cols];
+            var player1Row = 0;
+            var player1Col = 0;
+            var player2Row = 0;
+            var player2Col = 0;
+            const char player1Mark = 'f';
+            const char player2Mark = 's';
+            var IsDead = false;
+            const char dead = 'x';
+
+            for (int row = 0; row < rows; row++)
+            {
+                var line = Console.ReadLine();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = line[col];
+                    if (matrix[row, col] == 'f')
+                    {
+                        player1Row = row;
+                        player1Col = col;
+                    }
+                    if (matrix[row, col] == 's')
+                    {
+                        player2Row = row;
+                        player2Col = col;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+                var firstCommand = command[0];
+                var secondCommand = command[1];
+
+                if (firstCommand == "left")
+                {
+                    player1Col--;
+                    if (player1Col < 0)
+                    {
+                        player1Col = matrix.GetLength(1) - 1;
+                    }
+                    if (matrix[player1Row, player1Col] == player2Mark)
+                    {
+                        matrix[player1Row, player1Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player1Row, player1Col] = player1Mark;
+                    }
+                }
+                else if (firstCommand == "right")
+                {
+                    player1Col++;
+                    if (player1Col == matrix.GetLength(1))
+                    {
+                        player1Col = 0;
+                    }
+                    if (matrix[player1Row, player1Col] == player2Mark)
+                    {
+                        matrix[player1Row, player1Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player1Row, player1Col] = player1Mark;
+                    }
+                }
+                else if (firstCommand == "up")
+                {
+                    player1Row--;
+                    if (player1Row < 0)
+                    {
+                        player1Row = matrix.GetLength(0) - 1;
+                    }
+                    if (matrix[player1Row, player1Col] == player2Mark)
+                    {
+                        matrix[player1Row, player1Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player1Row, player1Col] = player1Mark;
+                    }
+                }
+                else if (firstCommand == "down")
+                {
+                    player1Row++;
+                    if (player1Row == matrix.GetLength(0))
+                    {
+                        player1Row = 0;
+                    }
+                    if (matrix[player1Row, player1Col] == player2Mark)
+                    {
+                        matrix[player1Row, player1Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player1Row, player1Col] = player1Mark;
+                    }
+                }
+
+                if (IsDead)
+                {
+                    PrintMatrix(matrix);
+                    return;
+                }
+
+                if (secondCommand == "left")
+                {
+                    player2Col--;
+                    if (player2Col < 0)
+                    {
+                        player2Col = matrix.GetLength(1) - 1;
+                    }
+                    if (matrix[player2Row, player2Col] == player1Mark)
+                    {
+                        matrix[player2Row, player2Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player2Row, player2Col] = player2Mark;
+                    }
+                }
+                else if (secondCommand == "right")
+                {
+                    player2Col++;
+                    if (player2Col == matrix.GetLength(1))
+                    {
+                        player2Col = 0;
+                    }
+                    if (matrix[player2Row, player2Col] == player1Mark)
+                    {
+                        matrix[player2Row, player2Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player2Row, player2Col] = player2Mark;
+                    }
+                }
+
+                else if (secondCommand == "up")
+                {
+                    player2Row--;
+                    if (player2Row < 0)
+                    {
+                        player2Row = matrix.GetLength(0) - 1;
+                    }
+                    if (matrix[player2Row, player2Col] == player1Mark)
+                    {
+                        matrix[player2Row, player2Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player2Row, player2Col] = player2Mark;
+                    }
+                }
+
+                else if (secondCommand == "down")
+                {
+                    player2Row++;
+                    if (player2Col == matrix.GetLength(0))
+                    {
+                        player2Col = 0;
+                    }
+                    if (matrix[player2Row, player2Col] == player1Mark)
+                    {
+                        matrix[player2Row, player2Col] = dead;
+                        IsDead = true;
+                    }
+                    else
+                    {
+                        matrix[player2Row, player2Col] = player2Mark;
+                    }
+                }
+
+                if (IsDead)
+                {
+                    PrintMatrix(matrix);
+                    return;
+                }
+
+            }
+        }
+        public static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
