059a6bd8920e9bcbb7ee3535b422b6053659086c
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-18 19:50:11 +0300

    Exam Preparation

---
+++ C# Advanced/ExamPreparation/03.SantasBagOfPresents/Bag.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Christmas
+{
+    public class Bag
+    {
+        private readonly List<Present> bag;
+        private string color;
+        private int capacity;
+
+        public Bag(string color, int capacity)
+        {
+            this.Color = color;
+            this.Capacity = capacity;
+            this.bag = new List<Present>();
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            private set { this.color = value; }
+        }
+
+        public int Capacity
+        {
+            get { return this.capacity; }
+            private set { this.capacity = value; }
+        }
+
+        public int Count
+        {
+            get { return this.bag.Count; }
+        }
+
+        public void Add(Present present)
+        {
+            if (this.bag.Count < this.Capacity)
+            {
+                this.bag.Add(present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            var isPresentExist = this.bag.Any(x => x.Name == name);
+            if (isPresentExist)
+            {
+                var presentToRemove = this.bag.Where(x => x.Name == name).FirstOrDefault();
+                this.bag.Remove(presentToRemove);
+            }
+            return isPresentExist;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            return this.bag.OrderByDescending(x => x.Weight).FirstOrDefault();
+        }
+
+        public Present GetPresent(string name)
+        {
+            return this.bag.Where(x => x.Name == name).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine(string.Format("{0} bag contains:", this.Color));
+
+            foreach (var present in this.bag)
+            {
+                sb.AppendLine(string.Format("{0}", present));
+            }
+            return sb.ToString().TrimEnd();
+        }
+
+
+    }
+}
---
+++ C# Advanced/ExamPreparation/03.SantasBagOfPresents/Present.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Christmas
+{
+    public class Present
+    {
+        private string name;
+        private double weight;
+        private string gender;
+
+        public Present(string name, double weight, string gender)
+        {
+            this.Name = name;
+            this.Weight = weight;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public double Weight
+        {
+            get { return this.weight; }
+            private set { this.weight = value; }
+        }
+
+        public string Gender
+        {
+            get { return this.gender; }
+            private set { this.gender = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Present {0} ({1}) for a {2}", this.Name
+                , this.Weight
+                , this.Gender);
+        }
+    }
+}
