405ad5103e2002d90c7961abd138158f4cfd8019
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-22 06:20:00 +0300

    Exam Preparation

---
+++ C# Advanced/ExamPreparation/03.Guild/Guild.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Guild
+{
+    public class Guild
+    {
+        private readonly string name;
+        private readonly int capacity;
+        private readonly List<Player> roster;
+
+
+        public Guild(string name, int capacity)
+        {
+            this.Name = name;
+            this.Capacity = capacity;
+            this.roster = new List<Player>();
+        }
+        public string Name { get; set; }
+
+
+        public int Capacity { get; set; }
+
+
+
+        public int Count
+        {
+            get { return this.roster.Count; }
+        }
+
+
+        public void AddPlayer(Player player)
+        {
+            if (this.roster.Count < this.Capacity)
+            {
+                this.roster.Add(player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            var isPlayerExist = this.roster.Any(x => x.Name == name);
+            if (isPlayerExist)
+            {
+                var playerToRemove = this.roster.Where(x => x.Name == name).FirstOrDefault();
+                this.roster.Remove(playerToRemove);
+                return isPlayerExist;
+            }
+            return isPlayerExist;
+
+        }
+
+        public void PromotePlayer(string name)
+        {
+            var playerToPromote = this.roster.Where(x => x.Name == name).FirstOrDefault();
+            if (playerToPromote.Rank != "Member")
+            {
+                playerToPromote.Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            var playerToDemote = this.roster.Where(x => x.Name == name).FirstOrDefault();
+            if (playerToDemote.Rank != "Trial")
+            {
+                playerToDemote.Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string playerClass)
+        {
+            var kickedPlayers = this.roster.Where(x => x.Class == playerClass).ToArray();
+            this.roster.RemoveAll(x => x.Class == playerClass);
+            return kickedPlayers;
+        }
+
+        public string Report()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Players in the guild: {this.Name}");
+            foreach (var player in roster)
+            {
+                sb.AppendLine($"{player}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+
+}
---
+++ C# Advanced/ExamPreparation/03.Guild/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    public class Player
+    {
+       
+        
+        public Player(string name, string playerClass)
+        {
+            this.Name = name;
+            this.Class = playerClass;
+        }
+        public string Name { get; set; }
+       
+
+        public string Class { get; set; }
+       
+
+        public string Rank { get; set; } = "Trial";
+       
+
+        public string Description { get; set; } = "n/a";
+        
+        //public void Promote() => this.Rank = "Member";
+       
+        //public void Demote() => this.Rank = "Trial";
+       
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Player {this.Name}: {this.Class}")
+                .AppendLine($"Rank: {this.Rank}")
+                .AppendLine($"Description: {this.Description}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
