68aaa31433482cdfa6d5049d381b448a6fea281d
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-27 15:19:01 +0300

    Exam Preparation

---
+++ C# Advanced/ExamPreparation/ClubParty/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClubParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var capacity = int.Parse(Console.ReadLine());
+            var input = new Stack<string>(Console.ReadLine().Split());
+
+            var hallsQueue = new Queue<string>();
+            var reservations = new Dictionary<string, List<int>>();
+
+            var peopleInHall = 0;
+            var hall = string.Empty;
+
+            while (input.Any())
+            {
+                var currentObject = input.Peek();
+                var digit = 0;
+                var isDigit = int.TryParse(currentObject, out digit);
+
+                if (!isDigit)
+                {
+                    input.Pop();
+                    hallsQueue.Enqueue(currentObject);
+                    if (!reservations.Any())
+                    {
+                        hall = hallsQueue.Dequeue();
+                        reservations[hall] = new List<int>();
+                    }
+                }
+                else
+                {
+                    if (!reservations.Any())
+                    {
+                        input.Pop();
+                        continue;
+                    }
+                    if (digit + peopleInHall <= capacity)
+                    {
+                        peopleInHall += digit;
+                        reservations[hall].Add(digit);
+                        input.Pop();
+                    }
+                    else
+                    {
+                        foreach (var reservation in reservations)
+                        {
+                            Console.Write($"{reservation.Key} -> ");
+                            Console.WriteLine(string.Join(", ", reservation.Value));
+                        }
+                        peopleInHall = 0;
+                        reservations.Clear();
+                    }
+                }
+                if (!reservations.Any())
+                {
+                    if (hallsQueue.Any())
+                    {
+                        hall = hallsQueue.Dequeue();
+                        reservations[hall] = new List<int>();
+                    }
+                    
+                }
+
+
+
+            }
+        }
+
+    }
+
+}
--- C# Advanced/ExamPreparation/LootBox/Program.cs
+++ C# Advanced/ExamPreparation/LootBox/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace LootBox
+namespace _01.LootBox
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var firstBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var secondBox = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var claimedItems = 0;
+
+            var firstBoxQueue = new Queue<int>(firstBox);
+            var secondBoxStack = new Stack<int>(secondBox);
+
+            while (firstBoxQueue.Any() && secondBoxStack.Any())
+            {
+                var first = firstBoxQueue.Peek();
+                var second = secondBoxStack.Peek();
+
+                if ((first + second) % 2 == 0)
+                {
+                    var sum = first + second;
+                    claimedItems += sum;
+                    firstBoxQueue.Dequeue();
+                    secondBoxStack.Pop();
+                }
+                else
+                {
+                    second = secondBoxStack.Pop();
+                    firstBoxQueue.Enqueue(second);
+                }
+            }
+
+            if (!firstBoxQueue.Any())
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            if (!secondBoxStack.Any())
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            if (claimedItems >= 100)
+            {
+                Console.WriteLine("Your loot was epic! Value: {0}", claimedItems);
+            }
+            else
+            {
+                Console.WriteLine("Your loot was poor... Value: {0}", claimedItems);
+            }
+
         }
     }
 }
