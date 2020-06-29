f623f99643beba298b17b00d03c59f0d55a5e995
BlagoKolev <blagovest.kolev@gmail.com>
2020-06-29 23:25:45 +0300

    HomeWork

---
+++ C# OOP/Inheritance/Person - Real Condition by Task/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Person
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("name", 50);
+            var person2 = new Person("name2", -2);
+            var child = new Child("name2", 12);
+
+            Console.WriteLine(person);
+            Console.WriteLine(person2);
+            Console.WriteLine(child);
+        }
+    }
+}
