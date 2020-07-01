4a7c7f5feb876a973e4723c2e87525dd38ce8df7
BlagoKolev <blagovest.kolev@gmail.com>
2020-07-01 20:14:47 +0300

    HomeWork

---
+++ C# OOP/Inheritance/Animals/Animal.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private const string ERROR_MESSAGE = "Invalid input!";
+        private string name;
+        private int age;
+        private string gender;
+
+        public Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception(ERROR_MESSAGE);
+                }
+                this.name = value;
+            }
+        }
+
+
+
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new Exception(ERROR_MESSAGE);
+                }
+                this.age = value;
+            }
+        }
+
+
+
+        public string Gender
+        {
+            get { return this.gender; }
+            set
+            {
+                if (value != "Male" && value != "Female")
+                {
+                    throw new Exception(ERROR_MESSAGE);
+                }
+                this.gender = value;
+            }
+        }
+
+
+        public abstract string ProduceSound();
+
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(string.Format("{0}", this.GetType().Name))
+            .AppendLine(string.Format("{0} {1} {2}", this.Name, this.Age, this.Gender))
+            .AppendLine(string.Format("{0}", this.ProduceSound()));
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C# OOP/Inheritance/Animals/StartUp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var animals = new List<Animal>();
+            Animal animal = null;
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "Beast!")
+                {
+                    break;
+                }
+
+                var animalArgs = Console.ReadLine().Split();
+
+                var name = animalArgs[0];
+                var age = int.Parse(animalArgs[1]);
+                var gender = animalArgs[2];
+
+                try
+                {
+                    if (command == "Dog")
+                    {
+                        animal = new Dog(name, age, gender);
+                    }
+
+                    else if (command == "Frog")
+                    {
+                        animal = new Frog(name, age, gender);
+                    }
+                    else if (command == "Cat")
+                    {
+                        animal = new Cat(name, age, gender);
+                    }
+                    else if (command == "Kitten")
+                    {
+                        animal = new Kitten(name, age);
+                    }
+                    else if (command == "Tomcat")
+                    {
+                        animal = new Tomcat(name, age);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+
+                animals.Add(animal);
+            }
+
+
+
+
+            Console.WriteLine(string.Join(Environment.NewLine, animals));
+        }
+    }
+}
