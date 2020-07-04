9f029cd0b44dc99768b08514ea04036ea76baa0c
BlagoKolev <blagovest.kolev@gmail.com>
2020-07-04 17:02:45 +0300

    HomeWork

---
+++ C# OOP/InterfacesAndAbstraction/MultipleImplementation/Citizen.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        public Citizen(string name, int age, string id, string birthday)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = id;
+            this.Birthdate = birthday;
+        }
+
+       
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
+    }
+}
