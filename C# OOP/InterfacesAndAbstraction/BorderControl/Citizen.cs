19563af4affd8014aa476390ed8afe53eed6ea90
BlagoKolev <blagovest.kolev@gmail.com>
2020-07-06 20:53:05 +0300

    HomeWork

---
+++ C# OOP/InterfacesAndAbstraction/BorderControl/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Citizen : IIdenticable
+    {
+        public Citizen(string name, int age, string iD)
+        {
+            Name = name;
+            Age = age;
+            ID = iD;
+        }
+
+        public string ID { get; private set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+    }
+}
---
+++ C# OOP/InterfacesAndAbstraction/BorderControl/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BorderControl
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var members = new List<IIdenticable>();
+
+            while (true)
+            {
+                var command = Console.ReadLine().Split();
+                if (command[0] == "End")
+                {
+                    break;
+                }
+                if (command.Length == 3)
+                {
+                    var name = command[0];
+                    var age = int.Parse(command[1]);
+                    var id = command[2];
+                    IIdenticable member = new Citizen(name, age,id);
+                    members.Add(member);
+                }
+                else
+                {
+                    var model = command[0];
+                    var id = command[1];
+                    IIdenticable member = new Robot(model, id);
+                    members.Add(member);
+                }
+               
+            }
+            var fakeId = Console.ReadLine();
+
+            foreach (var member in members)
+            {
+                if (member.ID.EndsWith(fakeId))
+                {
+                    Console.WriteLine(member.ID);
+                }
+            }
+        }
+    }
+}
