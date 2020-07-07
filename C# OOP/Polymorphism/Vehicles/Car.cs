0597de5c24ed3c24cd8365db7548c200091547da
BlagoKolev <blagovest.kolev@gmail.com>
2020-07-07 19:19:58 +0300

    HomeWork

---
+++ C# OOP/Polymorphism/Vehicles/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public class Car : Vehicle
+    {
+        private const double AIR_CONDITION_CONSUMPTION = 0.9;
+        public Car(double fuelQuantity, double fuelConsumption)
+            : base(fuelQuantity, fuelConsumption)
+        { }
+              
+        public override double FuelConsumption 
+        {
+            get { return base.FuelConsumption; }
+            set { base.FuelConsumption = value + AIR_CONDITION_CONSUMPTION; }
+        }
+                      
+        public override string ToString()
+        {
+            return string.Format($"{this.GetType().Name}: {this.FuelQuantity:f2}"); 
+        }
+    }
+}
---
+++ C# OOP/Polymorphism/Vehicles/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var carArgs = Console.ReadLine().Split();
+            var carFuel = double.Parse(carArgs[1]);
+            var carConsumption = double.Parse(carArgs[2]);
+            Vehicle car = new Car(carFuel, carConsumption);
+
+            var truckArgs = Console.ReadLine().Split();
+            var truckFuel = double.Parse(truckArgs[1]);
+            var truckConsumption = double.Parse(truckArgs[2]);
+            Vehicle truck = new Truck(truckFuel, truckConsumption);
+
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var command = Console.ReadLine().Split();
+
+                if (command[0] == "Drive")
+                {
+                    var distance = double.Parse(command[2]);
+                    if (command[1] == "Car")
+                    {
+                        Console.WriteLine(car.Drive(distance));
+                    }
+                    else if (command[1] == "Truck")
+                    {
+                        Console.WriteLine(truck.Drive(distance));
+                    }
+                }
+                else if (command[0] == "Refuel")
+                {
+                    var fuel = double.Parse(command[2]);
+
+                    if (command[1] == "Car")
+                    {
+                        car.Refuel(fuel);
+                    }
+                    else if (command[1] == "Truck")
+                    {
+                        truck.Refuel(fuel);
+                    }
+                }
+            }
+
+            Console.WriteLine(car);
+            Console.WriteLine(truck);
+        }
+    }
+}
