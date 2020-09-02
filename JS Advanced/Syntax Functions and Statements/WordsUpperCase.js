5adb3b8a933632314b3e92f9a1b2c22d45fb0ca6
BlagoKolev <blagovest.kolev@gmail.com>
2020-09-03 01:11:30 +0300

    HomeWork

---
+++ JS Advanced/Syntax Functions and Statements/WordsUpperCase.js
@@ -0,0 +1,15 @@
+let regEx = /[^0-9!?" ",.][A-Za-z]*/;
+
+function MakeItUpper(string){
+    let word = '' ;
+    let arrayOfWords = string.split(' ');
+    for (let index = 0; index < arrayOfWords.length; index++) {
+        word = arrayOfWords[index];
+        for (let index = 0; index < word.length; index++) {
+            
+            
+        }
+    }
+}
+
+MakeItUpper('Make it uppeR')
