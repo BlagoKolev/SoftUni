47a45a4b6454242eef842dc1ab9cb6fc3d71d2a5
BlagoKolev <blagovest.kolev@gmail.com>
2020-09-13 15:57:14 +0300

    HomeWork

---
+++ JS Advanced/Object and Classes/01.Rectangle.js
@@ -0,0 +1,13 @@
+class Rectangle{
+
+    constructor(width, height, color){
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea(){
+        let area = this.width * this.height;
+        return area;
+    }
+}
