18ddcbb208f891b4223c7df83cac91596376fb2d
BlagoKolev <blagovest.kolev@gmail.com>
2021-01-19 19:06:45 +0200

    Ex

---
+++ MS SQL/Table Relations/OnlineStore Database.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE	OnlineStore
+
+USE OnlineStore
+
+CREATE TABLE [ItemTypes](
+[ItemTypeID] INT PRIMARY KEY NOT NULL ,
+[Name] VARCHAR(50)
+);
+
+CREATE TABLE [Items](
+[ItemID] INT PRIMARY KEY NOT NULL ,
+[Name] VARCHAR(50) NOT NULL,
+[ItemTypeID] INT FOREIGN KEY REFERENCES [ItemTypes]([ItemTypeID])
+);
+
+CREATE TABLE [Cities](
+[CityID] INT PRIMARY KEY NOT NULL ,
+[Name] VARCHAR(50)
+);
+
+CREATE TABLE [Customers](
+[CustomerID] INT PRIMARY KEY NOT NULL ,
+[Name] VARCHAR(50) NOT NULL,
+[Birthday] DATE,
+[CityID] INT FOREIGN KEY REFERENCES [Cities]([CityID])
+);
+
+CREATE TABLE [Orders](
+[OrderID] INT PRIMARY KEY NOT NULL ,
+[CustomerID] INT FOREIGN KEY REFERENCES [Customers]([CustomerID])
+);
+
+CREATE TABLE [OrderItems](
+[OrderID] INT FOREIGN KEY REFERENCES [Orders]([OrderID]),
+[ItemID] INT FOREIGN KEY REFERENCES [Items]([ItemID])
+PRIMARY KEY ([OrderID],[ItemID])
+)
