603e5b5fdc771fef6a001bc5dfe701b51fd7f0e3
BlagoKolev <blagovest.kolev@gmail.com>
2021-01-19 19:08:37 +0200

    Ex

---
+++ MS SQL/Table Relations/Self-Referencing.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [Teachers](
+[TeacherID] INT PRIMARY KEY NOT NULL,
+[Name] NVARCHAR(50) NOT NULL,
+[ManagerID] INT FOREIGN KEY REFERENCES [Teachers]([TeacherID])
+);
+
+INSERT INTO [Teachers]
+VALUES
+(101, 'John', null),
+(102, 'Maya', 106),
+(103, 'Silvia', 106),
+(104, 'Ted', 105),
+(105, 'Mark', 101),
+(106, 'Greta', 101)
+
