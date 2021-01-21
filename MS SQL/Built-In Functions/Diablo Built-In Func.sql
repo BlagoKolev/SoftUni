3785825df437879e8207600c85bc654e9a205ab1
BlagoKolev <64737227+BlagoKolev@users.noreply.github.com>
2021-01-21 19:14:59 +0200

    Update Diablo Built-In Func.sql

--- MS SQL/Built-In Functions/Diablo Built-In Func.sql
+++ MS SQL/Built-In Functions/Diablo Built-In Func.sql
@@ -14,5 +14,22 @@ WHERE [IpAddress] LIKE '___.1%.%.___'
 ORDER BY [UserName]
 
 
+SELECT [Name] AS  [Game],
+CASE
+	WHEN DATEPART(HOUR,[Start]) >=0 AND DATEPART(HOUR,[Start]) < 12 THEN 'Morning'
+	WHEN DATEPART(HOUR,[Start]) >=12 AND DATEPART(HOUR,[Start]) < 18 THEN 'Afternoon'
+	WHEN DATEPART(HOUR,[Start]) >=18 AND DATEPART(HOUR,[Start]) < 24 THEN 'Evening'
+	END AS [Part Of The Day],
+CASE
+	WHEN [Duration] <=3 THEN 'Extra Short'
+	WHEN [Duration]>=4 AND [Duration] <=6 THEN 'Short'
+	WHEN [Duration] > 6 THEN 'Long'
+	ELSE  'Extra Long'
+	END AS [Duration]
+FROM [GAMES]
+ORDER BY [Game], [Duration], [Part Of The Day]
+
+
+
 
 
