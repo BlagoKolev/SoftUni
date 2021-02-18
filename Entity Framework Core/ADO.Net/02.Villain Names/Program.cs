b878df5f46fe4b367e9d04d906ed2e9f40147ee6
BlagoKolev <blagovest.kolev@gmail.com>
2021-02-18 18:44:44 +0200

    Exercise

---
+++ Entity Framework Core/ADO.Net/02.Villain Names/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace VillainNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Server=.;Database=MinionsDB;Integrated Security=true;";
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                string command = @"SELECT v.[Name], COUNT(m.[Name]) AS [Count]
+                                 FROM [MinionsVillains] AS mv 
+                                JOIN[Minions] AS m ON mv.MinionId = m.Id
+                                JOIN[Villains] As v ON mv.VillainId = v.Id 
+                                GROUP BY v.Name 
+                                HAVING COUNT(m.[Name]) > 3 
+                                ORDER BY COUNT(m.[Name]) DESC";
+
+                SqlCommand sqlCommand = new SqlCommand(command, sqlConnection);
+                using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                {
+             
+                    while (reader.Read())
+                    {
+                        string name = (string)reader["Name"];
+                        int count = (int)reader["Count"];
+                        Console.WriteLine($"{name} - {count}");
+                       
+                    }
+                }
+            };
+
+
+        }
+    }
+}
