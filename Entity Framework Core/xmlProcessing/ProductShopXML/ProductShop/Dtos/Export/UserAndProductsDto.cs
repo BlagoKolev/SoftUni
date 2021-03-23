9b4339e359e4ea71060e56f9a4621f2a6149ba4c
BlagoKolev <blagovest.kolev@gmail.com>
2021-03-23 06:56:20 +0200

    Working with XML - Import and Export

---
+++ Entity Framework Core/xmlProcessing/ProductShopXML/ProductShop/Dtos/Export/UserAndProductsDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop.Dtos.Export
+{
+    using System.Xml.Serialization;
+
+    public class UserAndProductsDto
+    {
+        [XmlElement("count")]
+        public int Count { get; set; }
+
+        [XmlArray("users")]
+        public UserWithProductsDto[] Users { get; set; }
+    }
+}
